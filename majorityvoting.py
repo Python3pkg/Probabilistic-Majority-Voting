395c974928915f51463d95dbb949fa904ccf360f
TPeterW <peterwangtao0@hotmail.com>
2016-11-28 01:00:53 -0500

    It looks more like voting now

--- majorityvoting.py
+++ majorityvoting.py
@@ -21,31 +21,58 @@ class MajorityVoting(object):
         results = []
 
         # first couple windows
-        # TODO:
+        # TODO: go with popular votes
 
         # middle part
-        for w in range(2, len(self.probabilities) - 2):         # window for which to calculate the voting results
-            cut = self.probabilities[w - (windows - 1) // 2 : w + (windows - 1) // 2 + 1]       # all voters
+        for w in range((windows - 1) // 2, len(self.probabilities) - (windows - 1) // 2):           # window for which to calculate the voting results
+            cut = self.probabilities[w - (windows - 1) // 2 : w + (windows - 1) // 2 + 1]           # all voters
             results.append(self.get_majority(cut, windows))
-            return
+            print(results)
+            # TODO: delete
+            break
 
         # last couple windows
-        # TODO:
+        # TODO: go with popular votes
 
         return results
 
     def get_majority(self, cut, windows):
-        single_window = [[]] * windows
+        single_window = [[] for i in range(windows)]
+        cats = len(cut[0])                                          # categories
 
         for voter in cut:
             max = -sys.maxsize - 1
-            for i in range(len(voter)):
+            for i in range(cats):
                 if voter[i] > max:
                     max = voter[i]
-                single_window[voter.index(max)].append(voter)           # append to respective windows
-            
-        # TODO:
-        # print(single_window)
+            single_window[voter.index(max)].append(voter)           # append to respective windows
+        
+        len_list = [[] for i in range(windows)]                     # number of voters for each category
+        for i in range(windows):
+            len_list[i] = len(single_window[i])
+        len_list = sorted(len_list, reverse=True)                   # descending order
+
+        if len_list[0] == len_list[1]:                              # more than one majority
+            # when electoral collage fails, we count on popular votes
+            popvotes = [0] * cats
+            for voter in cut:
+                for i in range(cats):
+                    popvotes[i] += voter[i]
+            maxvotes = 0
+            maxindex = 0
+            for i in range(cats):
+                if popvotes[i] > maxvotes:
+                    maxvotes = popvotes[i]
+                    maxindex = i
+            return maxindex
+        else:                                                        # clear winner
+            maxlen = 0
+            maxindex = 0
+            for i in range(len(single_window)):
+                if len(single_window[i]) > maxlen:
+                    maxlen = len(single_window[i])
+                    maxindex = i
+            return maxindex
 
     def check_arguments(self):
         if type(self.probabilities) is not list or type(self.categories) is not list:
