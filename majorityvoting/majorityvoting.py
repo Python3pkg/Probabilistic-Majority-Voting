8a4162a97f202033dfd11f92bbe49c8d9f3d0869
TPeterW <peterwangtao0@hotmail.com>
2016-12-14 20:21:41 -0500

    Added a lot of things

--- majorityvoting/majorityvoting.py
+++ majorityvoting/majorityvoting.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+__author__      = 'Tao Peter Wang'
+__version__     = '0.1.2'
+__license__     = 'MIT'
+__email__       = 'peterwangtao0@hotmail.com'
+__date__        = 'Dec-14-2016'
+
 import sys
 import numpy as np
 
